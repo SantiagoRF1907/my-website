afbbd0c2290faf932562e8a7e6e6094dd262c152
SantiagoRF1907 <santiago.rferraez@gmail.com>
2024-12-23 01:29:20 -0800

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+document.getElementById("about-me-nav").addEventListener("click", () => {
+  document
+    .getElementById("about-me-container")
+    .scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("technologies-nav").addEventListener("click", () => {
+  document
+    .getElementById("technologies-container")
+    .scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("my-projects-nav").addEventListener("click", () => {
+  document
+    .getElementById("my-projects-container")
+    .scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("my-hobbies-nav").addEventListener("click", () => {
+  document
+    .getElementById("my-hobbies-container")
+    .scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("social-media-nav").addEventListener("click", () => {
+  document
+    .getElementById("social-media-container")
+    .scrollIntoView({ behavior: "smooth" });
+});
